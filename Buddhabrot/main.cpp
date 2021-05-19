7066e0afafd469ba7e28f4eba39f441e4e8c66c1
Przemysław Papla <przemekpapla@gmail.com>
2021-05-19 16:53:45 +0200

    total time cpu

--- Buddhabrot/main.cpp
+++ Buddhabrot/main.cpp
@@ -3,7 +3,9 @@
 #include "cuda_runtime.h"
 #include <string>
 #include <stdio.h>
+#include <chrono>
 #include "BuddhabrotRenderer.h"
+#include <iostream>
 
 cudaError_t initCuda() {
 	cudaError_t cudaStatus;
@@ -21,7 +23,7 @@ int main() {
 	}
     uint32_t width = 1920;
     uint32_t height = 1080;
-    uint64_t samples = 100000000;
+    uint64_t samples = 20000000;
 	float start_re = 0.f;
 	float start_im = 0.f;
 	float background_r = 0.f;
@@ -33,11 +35,15 @@ int main() {
 	float scale = 3.f;
 	float rotation = 90.f * M_PI / 180.f;
 	std::string filename = "buddhabrot";
+
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     BuddhabrotViewport viewport(width, height, center_re, center_im, scale, rotation);
     BuddhabrotRenderer renderer(viewport, false, 0, 0, 0, start_re, start_im);
     renderer.addLayer(samples, 800, 1, 0, 0, 2, 1);
     renderer.addLayer(samples, 200, 0, 1, 0, 2, 1);
     renderer.addLayer(samples, 50, 0, 0, 1, 2, 1);
     renderer.saveImage(filename);
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Total time (CPU): " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms" << std::endl;
     return 0;
 }
